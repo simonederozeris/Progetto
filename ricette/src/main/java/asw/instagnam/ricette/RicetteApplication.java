35109483b6a8e3d0e18a01bd1651fcd029d496c1
simone.derozeris <simone.derozeris@gmail.com>
2020-05-27 20:49:04 +0200

    prova commit

--- ricette/src/main/java/asw/instagnam/ricette/RicetteApplication.java
+++ ricette/src/main/java/asw/instagnam/ricette/RicetteApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient 
 public class RicetteApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RicetteApplication.class, args);
 	}
