57592800afc9c4f81b46e8a0e2597c7bdb4acc8a
Luca <Luca@192.168.0.104>
2020-05-28 19:34:00 +0200

    Primo commit Luca

--- ricette/src/main/java/asw/instagnam/ricette/RicetteApplication.java
+++ ricette/src/main/java/asw/instagnam/ricette/RicetteApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient 
 public class RicetteApplication {
+	//Classe per le ricette
 	public static void main(String[] args) {
 		SpringApplication.run(RicetteApplication.class, args);
 	}
